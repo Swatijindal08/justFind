4347a3452c396226994646b4b22f9df2a87958d5
swati Jindal <swatijindal2018@gmail.com>
2021-05-23 19:14:24 +0530

    progress

--- jobPortal/urls.py
+++ jobPortal/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('',home,name='home'),
+    path('login/',loginUser,name='login'),
+    path('logout/',logoutUser,name='logout'),
+    path('register/',registerUser,name='register'),
+    path('apply/',applyPage,name='apply'),
     
 ]
--- justFind/settings.py
+++ justFind/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobPortal.apps.JobportalConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,7 +121,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
