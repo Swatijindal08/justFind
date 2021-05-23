6aaa83bc082f2af651132138c19761f5001c8cd6
swati Jindal <swatijindal2018@gmail.com>
2021-05-24 00:11:53 +0530

    work till now

--- justFind/urls.py
+++ justFind/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('jobPortal.urls'))
+    path('',include('jobPortal.urls')),
 ] 
 
 if settings.DEBUG:
